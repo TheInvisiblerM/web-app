b8ca97e1f8645f023719bd6090d6e05d3d4d600f
TheInvisiblerM <theinvisiblerm6810@gmail.com>
2025-12-09 17:20:57 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
-});
+  base: './', // مهم عشان Manifest يشتغل صح على Vercel
+})
