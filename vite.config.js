d50de0f5666894fb7b5d70eea4d0fb1853db9209
TheInvisiblerM <theinvisiblerm6810@gmail.com>
2025-12-10 16:46:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // مهم عشان Manifest يشتغل صح على Vercel
+  base: './',
+  optimizeDeps: {
+    include: ['xlsx']  // مهم عشان Vite يحللها صح
+  }
 })
