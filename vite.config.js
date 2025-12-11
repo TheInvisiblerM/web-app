d591c1290721fb4cce1158c2f485161884f68803
TheInvisiblerM <theinvisiblerm6810@gmail.com>
2025-12-11 20:22:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  optimizeDeps: {
-    include: ['xlsx']  // مهم عشان Vite يحللها صح
-  }
+  base: '/'
 })
